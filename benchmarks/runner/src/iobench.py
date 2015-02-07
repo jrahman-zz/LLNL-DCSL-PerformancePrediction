19a2d8e97b1b213ceab6fe422a5c5692e5514065
Jason P. Rahman <rahmanj@purdue.edu>
2015-02-06 22:39:17 -0500

    Updates to iobench suite

--- benchmarks/runner/src/iobench.py
+++ benchmarks/runner/src/iobench.py
@@ -4,8 +4,9 @@ from program import Program
 from interference import InterferenceThread
 
 class IOBenchmark(Program):
+    """ Base class for IOBench benchmarks """
 
-    def __init__(self, read=True, seconds=5, size='1K', cores=[0]):
+    def __init__(self, seconds=5, read=True, size='1K', path='/tmp/iobench.file', cores=[0]):
         Program.__init__(self, cores)
         self._read = read
         if read:
@@ -16,7 +17,7 @@ class IOBenchmark(Program):
         self._seconds = seconds
         self._cmd = 'java'
         self._runstring = ','.join([self._operation, '1', self._size, self._size])
-        self._params = ['-classpath', 'iobench.jar', 'Main', 'run', '-h=20', '/tmp/iobench.file', str(self._seconds), self._runstring]
+        self._params = ['-classpath', 'iobench.jar', 'Main', 'run', '-h=20', path, str(self._seconds), self._runstring]
         self._name = 'iobench_%s_%s' % (self._operation, self._size)
 
     def _setup(self):
@@ -26,7 +27,7 @@ class IOBenchmark(Program):
         pass
 
     def _process_output(self, output):
-        regex = r"Total for \[%s,%s,%s\]: \d+\.\d+ events \(\d+\.\d+ errors\), mean (\d*\.\d*)ms, 10th (\d*\.\d*)ms, 50th (\d*\.\d*)ms, 90th (\d*\.\d*)ms, 99th (\d*\.\d*)ms, 999th \d*\.\d*ms, 9999th \d*\.\d*ms, min (\d*\.\d*)ms, max (\d*\.\d*)ms$"
+        regex = r"Total for \[%s,%s,%s\]: \d+\.\d+ events \(\d+\.\d+ errors\), mean (\d*\.\d*)ms, 10th (\d*\.\d*)ms, 50th (\d*\.\d*)ms, 90th (\d*\.\d*)ms, 99th (\d*(?:\.\d*){0,1})ms, 999th (?:\d*(?:\.\d*){0,1})ms, 9999th (?:\d*(?:\.\d*){0,1})ms, min (\d*\.\d*)ms, max (\d*(?:\.\d*){0,1})ms$"
         regex = regex % (self._operation.lower(), self._size, self._size)
         result = re.search(regex, output)
         if result == None:
@@ -44,8 +45,9 @@ class IOBenchmark(Program):
         return features
 
 class IOBenchInterfere(InterferenceThread):
+    """ Base class for IOBench interference threads"""
 
-    def __init__(self, read=True, seconds=30, size='1K', cores=[0]):
+    def __init__(self, seconds=30, read=True, size='1K', path='/tmp/iobench.file', cores=[0]):
         InterferenceThread.__init__(self, cores)
         self._size = size
         self._seconds = seconds
@@ -57,61 +59,61 @@ class IOBenchInterfere(InterferenceThread):
         self._name = 'iobench_%s_%s' % (self._operation, self._size)
         self._cmd = 'java'
         self._runstring = ','.join([self._operation, '1', self._size, self._size])
-        self._params = ['-classpath', 'iobench.jar', 'Main', 'run', '-h=20', '/tmp/iobench.file', str(self._seconds), self._runstring]
-
-class IOBenchRead1K(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 5, True, '1K', cores)
-
-class IOBenchRead1KInterfere(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, True, '1K', cores)
+        self._params = ['-classpath', 'iobench.jar', 'Main', 'run', '-h=20', path, str(self._seconds), self._runstring]
 
 class IOBenchRead1M(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 10, True, '1M', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 10, True, '1M', path, cores)
 
 class IOBenchRead1MInterfere(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, True, '1M', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, True, '1M', path, cores)
+
+class IOBenchRead4M(IOBenchmark):
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 15, True, '4M', path, cores)
+
+class IOBenchRead1MInterfere(IOBenchInterfere):
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, True, '4M', path, cores)
 
 class IOBenchRead1G(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 20, True, '1G', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 20, True, '1G', path, cores)
 
 class IOBenchRead1GInterfere(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, True, '1G', cores)
-
-class IOBenchWrite1K(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 5, False, '1K', cores)
-
-class IOBenchWrite1KInterfere(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, False, '1K', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, True, '1G', path, cores)
 
 class IOBenchWrite1M(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 10, False, '1M', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 10, False, '1M', path, cores)
 
-class IOBenchWrite1MInterfre(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, False, '1M', cores)
+class IOBenchWrite1MInterfere(IOBenchInterfere):
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, False, '1M', path, cores)
+
+class IOBenchWrite4M(IOBenchmark):
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 15, False, '4M', path, cores)
+
+class IOBenchWrite4MInterfre(IOBenchInterfere):
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, False, '4M', path, cores)
 
 class IOBenchWrite1G(IOBenchmark):
-    def __init__(self, cores=[0]):
-        IOBenchmark.__init__(self, 20, False, '1G', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchmark.__init__(self, 20, False, '1G', path, cores)
 
 class IOBenchWrite1GInterfere(IOBenchInterfere):
-    def __init__(self, cores=[0]):
-        IOBenchInterfere.__init__(self, 30, False, '1G', cores)
+    def __init__(self, path, cores=[0]):
+        IOBenchInterfere.__init__(self, 60, False, '1G', path, cores)
 
 if __name__ == '__main__':
 
     test = """Histogram 10 for [write,1M,1M]: 1.0 events (0.0 errors), mean 71.0ms, 10th 71.0ms, 50th 71.0ms, 90th 71.0ms, 99th 71.0ms, 999th 71.0ms, 9999th 71.0ms, min 71.0ms, max 71.0ms
 Total for [write,1M,1M]: 173.0 events (0.0 errors), mean 29.8ms, 10th 5.4ms, 50th 20.8ms, 90th 62.9ms, 99th 207ms, 999th 217ms, 9999th 217ms, min 4.63ms, max 217ms"""
 
-    m = IOBenchWrite1M()
+    m = IOBenchWrite1M('/tmp/iobench.file')
     result = m._process_output(test)
     print result

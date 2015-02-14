f36add717df7ed49cff410eb4faa28de7fa94556
Jason P. Rahman <rahmanj@purdue.edu>
2015-02-13 20:55:53 -0500

    Last ~10 commits actually happened today (2/14)

---
+++ applications/voldemort/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+usage() {
+    echo "Usage: run.sh YCSB_DIR [OPERATION_COUNT]"
+}
+
+if [ $# -eq 1 ]; then
+    YCSB_DIR=${1}
+    OPERATIONS=""
+elif [ $# -eq 2 ]; then
+    YCSB_DIR=${1}
+    OPERATIONS="-P \"operationcount=${2}\""
+else
+    usage
+    exit 1
+fi
+
+if [ ! -x "$YCSB_DIR}/bin/ycsb" ]; then
+    echo "Error: Invalid YCSB directory"
+    usage
+    exit 1
+fi
+
+PARAMS='-P "hosts=localhost" ${OPERATIONS}'
+
+echo "Run: Starting now..."
+ycsb_run.sh "${YCSB_DIR}" 'cassandra-10' ${PARAMS}
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to launch the run"
+    exit 1
+fi
+echo "Run: Completed"
+
+exit 0

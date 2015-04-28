32a50d7d5e3abafb759c4876e677af6c1c374150
jrahman <rahmanj@purdue.edu>
2015-04-27 23:19:28 -0700

    New scheduler work

---
+++ scheduler/measurement_manager.py
@@ -0,0 +1,64 @@
+import gevent
+import logging
+from datetime import datetime, timedelta
+from automation.runner.load_benchmarks import load_benchmarks
+
+
+class MeasurementManager:
+
+	def __init__(self, environ):
+		self._benchmarks = load_benchmarks(environ)
+	
+		self._start_time = None
+		self._end_time = None
+		self._is_benchmarking = None
+
+		self._benchmarks_used = Set()
+
+		# Keyed by name, then a list
+		self._latest_measurements = {}
+		# Keyed by time, then 
+		self._measurements = {}
+		
+		self._loopdelta
+
+		self._keep_looping = False
+		self._looper = None
+
+	def run_benchmarks(self):
+		self._is_benchmarking = True
+		self._start_time = datetime.utcnow()
+		logging.info('Starting benchmarks at %s', self._start_time)
+
+		self._finish_time = datatime.utcnow()
+		logging.info('Ending benchmarks at %s', self._end_time)
+		pass
+
+	def start_loop(self):
+		""" Start benchmarking loop """
+		self._keep_looping = True
+		self._looper = gevent.spawn(MeasurementManager._loop_main, self)
+		self._looper.start()
+
+	def end_loop(self):
+		""" End benchmarking loop """
+		pass
+
+	def _run_benchmarks(self):
+		pass
+
+	def _loop_main(self):
+		logging.info('MeasurementManager: Loop main starting')	
+		# TODO
+		while self._keep_looping:
+			gevent.sleep(100)
+
+		logging.info('MeasurementManager: Loop main ending')
+
+	def _insert_measurement(self, key, value, time):
+		if key not in self._latest_measurement:
+			self._latest_measurement[key] = []
+		self._latest_measurements[key].append(value)
+		if time not in self._measurements:
+			self._measurements[time] = dict()
+		self._measurements[time][key] = value
--- scheduler/worker_daemon.py
+++ scheduler/worker_daemon.py
@@ -4,17 +4,48 @@ import os
 
 import signal
 
-import get_load
 from datetime import datetime
 
 from flask import Flask, jsonify, request, json
-import daemon_helpers as helpers
+
+import scheduler.get_load
+import scheduler.daemon_helpers as helpers
+from scheduler.job_manager import JobManager
+from scheduler.measurement_manager import MeasurementManager
 
 import gevent
 from gevent.pywsgi import WSGIServer
 from gevent import subprocess
 
+import automation.runner.load_environ
+
 app = Flask(__name__)
+
+def measurement():
+	""" Perform a status measurement """
+	measurement = get_load.get_load()
+	response = helpers.update_worker(sys.argv[1], message))
+
+def job_completed(job_type, job_id, start_time, end_time, exit_code):
+	""" Take action when a job completes according to the job manager """
+	# TODO
+	pass
+
+environ = None
+job_manager = None
+measurement_manager = None
+
+def init_server(base_path, modules):
+	global environ
+	global job_manager
+	global measurement_manager
+
+	modules = ['%s/%s' % (base_path, module) for module in modules]
+	environ = load_environ('%s/config.json' % (base_path), modules)
+	job_manager = JobManager(environ)
+	job_manager.register_completion_cb(job_completed)
+	measurement_manager = MeasurementManager(environ)
+	
 
 @app.route('/')
 def root():
@@ -22,13 +53,19 @@ def root():
 
 @app.route(helpers.START_JOB_ENDPOINT, methods=['POST'])
 def start_job():
-	# TODO
-	return local_id
+	message = request.json
+	helpers.log_rpc(message)
+	data = message['data']
+	return job_manager.start_job(data)
 
 # Configure error handlers
 @app.errorhandler(404)
 def not_found_error(e):
     helper.not_found(e.args[0])
+
+@app.errorhandler(ValueError)
+def value_error(e):
+	helper.bad_request(e.args[0])
 
 keep_alive = True
 
@@ -45,22 +82,15 @@ def signal_handler(signo, stack):
 
 
 def notify_master():
+	""" Indicate that we have just started normal operation """
 	processors = subprocess.check_output('cat /proc/cpuinfo | grep processor | wc -l', shell=True)
-	
 	message = {}
 	message['processors'] = int(processors)
 	message['memory'] = get_load.get_free_memory()
-
 	response = helpers.update_worker(sys.argv[1], message)
 
-def measurement():
-	measurement = get_load.get_load()
-	response = helpers.update_worker(sys.argv[1], message))
-
 if __name__ == '__main__':
-
 	signal.signal(signal.SIGTERM, signal_handler)
-
 	logging.basicConfig(level=logging.INFO)
 
 	logging.info('Starting server...')

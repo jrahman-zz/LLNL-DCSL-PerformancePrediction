3c9741705c05678f1e654b04a1d381f0737febda
Subrata Mitra <mitra3@llnl.gov>
2015-10-31 22:33:49 -0700

    Some fixes in the script as suggested by Jason

--- single_app_contention/build_benchmarks.sh
+++ single_app_contention/build_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for BENCHMARK in `cat parsec_benchmarks`; do
+for BENCHMARK in `cat manifests/parsec_benchmarks`; do
 	echo "Building parsec_${BENCHMARK}..."
 	parsecmgmt -a build -p "${BENCHMARK}" 2>&1 > output
 	if [ $? -ne 0 ]; then
@@ -10,7 +10,7 @@ for BENCHMARK in `cat parsec_benchmarks`; do
 	echo "Built parsec_${BENCHMARK}"
 done
 
-for BENCHMARK in `cat spec_int_benchmarks`; do
+for BENCHMARK in `cat manifests/spec_int_benchmarks`; do
 	echo "Building spec_int_${BENCHMARK}..."
 	runspec --action setup --config research_config --size ref "${BENCHMARK}" 2>&1 > output
 	if [ $? -ne 0 ]; then
@@ -20,7 +20,7 @@ for BENCHMARK in `cat spec_int_benchmarks`; do
 	echo "Built spec_int_${BENCHMARK}"
 done
 
-for BENCHMARK in `cat spec_fp_benchmarks`; do
+for BENCHMARK in `cat manifests/spec_fp_benchmarks`; do
 	echo "Building spec_fp_${BENCHMARK}..."
 	runspec --action setup --config research_config --size ref "${BENCHMARK}" 2>&1 > output
 	if [ $? -ne 0 ]; then

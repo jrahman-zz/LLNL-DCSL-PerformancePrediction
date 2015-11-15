7d5f5bdc7b638ead05cf623423567abf06fe0d65
jrahman <rahmanj@purdue.edu>
2015-11-15 14:15:56 -0800

    Update to use pre-packaged YCSB instead of fussing with an installation from source

--- install_mongodb_ycsb.sh
+++ install_mongodb_ycsb.sh
@@ -12,10 +12,21 @@ fi
 
 CWD=`pwd`
 
-if [ ! -d "$YCSBDIR" ]; then
-	  # Control will enter here if $YCSBDIR doesn't exist.
-	  cd $APPS_DIR
-	  git clone git://github.com/brianfrankcooper/YCSB.git
-	  cd YCSB
-	  "${CWD}/mvn" -pl \!cassandra2,\!gemfire,\!hbase094,\!hbase098,\!hbase10,\!kudu,\!distribution clean package 
+YCSBDIR="${APPS_DIR}/ycsb-0.3.1"
+if [ ! -d "${YCSBDIR}" ]; then
+    wget http://github.com/brianfrankcooper/YCSB/releases/download/0.3.1/ycsb-0.3.1.tar.gz
+    tar -C "${APPS_DIR}" -zxvf ycsb-0.3.1.tar.gz
+    if [ $? -ne 0 ]; then
+        echo "Error: Failed to untar YCSB"
+        exit 1
+    fi
+    rm ycsb-0.3.1.tar.gz
 fi
+
+#if [ ! -d "$YCSBDIR" ]; then
+	  # Control will enter here if $YCSBDIR doesn't exist.
+#	  cd $APPS_DIR
+#	  git clone git://github.com/brianfrankcooper/YCSB.git
+#	  cd YCSB
+#	  "${CWD}/mvn" -pl \!cassandra2,\!gemfire,\!hbase094,\!hbase098,\!hbase10,\!kudu,\!distribution clean package 
+#fi

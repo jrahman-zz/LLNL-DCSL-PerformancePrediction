8408df71ab5360f6e78d7d65e50c44cecd4752a8
jrahman <rahmanj@purdue.edu>
2015-11-15 13:52:58 -0800

    Installation script update

--- install_maven.sh
+++ install_maven.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-APPS_DIR=/p/lscratche/mitra3/apps
+APPS_DIR=/p/lscratche/${USER}/apps
+CWD=`pwd`
 
-wget http://ftp.heanet.ie/mirrors/www.apache.org/dist/maven/maven-3/3.1.1/binaries/apache-maven-3.1.1-bin.tar.gz
+#wget http://ftp.heanet.ie/mirrors/www.apache.org/dist/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
+wget https://www.apache.org/dist/maven/binaries/apache-maven-3.2.1-bin.tar.gz
 tar xzf apache-maven-*-bin.tar.gz -C $APPS_DIR
-cd $APPS_DIR
-ln -s apache-maven-* maven
+ln -s "${APPS_DIR}/apache-maven"-*/bin/mvn "${CWD}/mvn"
 
 
--- install_mongodb_ycsb.sh
+++ install_mongodb_ycsb.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-APPS_DIR=/p/lscratche/mitra3/apps
-MONGODIR=$APPS_DIR/mongodb-linux-x86_64-2.6.7
+APPS_DIR=/p/lscratche/${USER}/apps
+MONGODIR=$APPS_DIR/mongodb-linux-x86_64-3.0.7
 YCSBDIR=$APPS_DIR/YCSB
 
 if [ ! -d "$MONGODIR" ]; then
-	  # Control will enter here if $MONGODIR doesn't exist.
+    # Control will enter here if $MONGODIR doesn't exist.
+    echo "Error: MongoDB missing"
+    exit 1
 fi
+
+CWD=`pwd`
 
 if [ ! -d "$YCSBDIR" ]; then
 	  # Control will enter here if $YCSBDIR doesn't exist.
 	  cd $APPS_DIR
 	  git clone git://github.com/brianfrankcooper/YCSB.git
 	  cd YCSB
-	  mvn clean package
+	  "${CWD}/mvn" -pl \!cassandra2,\!gemfire,\!hbase094,\!hbase098,\!hbase10,\!kudu,\!distribution clean package 
 fi
-
-

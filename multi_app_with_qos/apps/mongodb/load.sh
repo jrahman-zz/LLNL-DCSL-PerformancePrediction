58a4217ddd3991d2eeba8910b12e7cd7f09f8099
jrahman <rahmanj@purdue.edu>
2015-11-15 14:15:02 -0800

    Updates to driver script to reflect new YCSB run instructions

--- multi_app_with_qos/apps/mongodb/load.sh
+++ multi_app_with_qos/apps/mongodb/load.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 APPS_DIR=/p/lscratche/${USER}/apps
-YCSB_DIR=$APPS_DIR/YCSB
+YCSB_DIR=$APPS_DIR/ycsb-0.3.1
 
 WORKLOAD=$1
 
 echo "**** Loading MongoDB with YCSB **** with workload = ${WORKLOAD}"
-${YCSB_DIR}/bin/ycsb load mongodb -s -P "${YCSB_DIR}/workloads/${WORKLOAD}"
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load mongodb -s -P "${YCSB_DIR}/workloads/${WORKLOAD}"
--- multi_app_with_qos/apps/mongodb/run_driver.sh
+++ multi_app_with_qos/apps/mongodb/run_driver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 APPS_DIR=/p/lscratche/${USER}/apps
-YCSB_DIR=$APPS_DIR/YCSB
+YCSB_DIR=$APPS_DIR/ycsb-0.3.1
 
 WORKLOAD=$1
 OUTPUT_PATH=$2
 
 echo "**** Running MongoDB with YCSB **** with workload = ${WORKLOAD}"
-${YCSB_DIR}/bin/ycsb run mongodb -s -P ${WORKLOAD} 2>&1 > ${OUTPUT_PATH}
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run mongodb -s -P ${WORKLOAD} 2>&1 > ${OUTPUT_PATH}

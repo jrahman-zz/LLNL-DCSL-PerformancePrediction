deb38b7e14ac98395554353e43f39803d9f1f265
jrahman <rahmanj@purdue.edu>
2015-11-16 21:58:19 -0800

    Update create_experiment.py to allow duplicate copies of the same application

--- multi_app_contention/create_experiment.py
+++ multi_app_contention/create_experiment.py
@@ -1,6 +1,6 @@
 #!/bin/env python
 
-import itertools
+#import itertools
 import sys
 
 def read_applications(cores):
@@ -17,20 +17,35 @@ def read_applications(cores):
                 applications.append([suite, line.strip(), str(app_cores)])
     return applications
 
-def create_output_path(combination, rep):
-    path = 'data/' + str(len(combination)) + '_' + '.'.join([ "_".join(app) for app in combination])
+def create_output_path(app_count, apps, rep):
+    path = 'data/' + str(app_count) + apps
     path += '.%(rep)d.reporter.perf_counters' % locals()
     return path
+
+def decode_apps(idx, apps, app_count):
+    app_list = []
+    while app_count > 0:
+        app_list.append(apps[idx % len(apps)])
+        idx = int(idx / len(apps))
+        app_count -= 1
+    return sorted(app_list)
+
+def get_apps(app_count, applications):
+    apps = dict()
+    for i in range(len(applications)**app_count):
+        app_list = decode_apps(i, applications, app_count)
+        key = " ".join([" ".join(app) for app in app_list])
+        value =  ".".join(["_".join(app) for app in app_list]) 
+        apps[key] = value
+    return apps
 
 def main(reps, cores, maxapps):
     applications = read_applications(cores)
     for rep in range(reps):
         for app_count in range(2, maxapps + 1):
-            for combination in itertools.combinations(applications, app_count):
-                combination = sorted(combination) # Force sorted order for consistency
-                combo = " ".join([" ".join(app) for app in combination])
-                output = create_output_path(combination, rep)
-                print('%(combo)s %(output)s %(rep)d' % locals())
+            for apps, apps_dot in get_apps(app_count, applications).items():
+                output = create_output_path(app_count, apps_dot, rep)
+                print('%(apps)s %(output)s %(rep)d' % locals())
 #
 # Parameters: create_experiments.py REPS CORES MAXAPPS
 #

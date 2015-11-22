47e1ad46b4f2bdfd29bb35fa4f744ea7e936e496
jrahman <rahmanj@purdue.edu>
2015-11-22 14:08:41 -0800

    Update MongoDB harness scripts to use YCSB 0.4.0

--- multi_app_with_qos/apps/mongodb/load.sh
+++ multi_app_with_qos/apps/mongodb/load.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 APPS_DIR=/p/lscratche/${USER}/apps
-YCSB_DIR=$APPS_DIR/ycsb-0.3.1
+YCSB_DIR=$APPS_DIR/ycsb-0.4.0
 
 WORKLOAD=$1
 
 echo "**** Loading MongoDB with YCSB **** with workload = ${WORKLOAD}"
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load mongodb -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=100000 -p operationcount=1000000 2>/dev/null 1>/dev/null
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load mongodb -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 2>/dev/null 1>/dev/null
--- multi_app_with_qos/apps/mongodb/run_driver.sh
+++ multi_app_with_qos/apps/mongodb/run_driver.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 APPS_DIR=/p/lscratche/${USER}/apps
-YCSB_DIR=$APPS_DIR/ycsb-0.3.1
+YCSB_DIR=$APPS_DIR/ycsb-0.4.0
 
 WORKLOAD=$1
 OUTPUT_PATH=$2
 
+# Filter irrelevant output we don't care about
 PATTERN="\[OVERALL\]|\[CLEANUP\]|\[READ\]|\[UPDATE\]"
 
 echo "**** Running MongoDB with YCSB **** with workload = ${WORKLOAD}"
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run mongodb -s -threads 2 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=100000 -p operationcount=1000000 2> /dev/null | egrep "${PATTERN}" > ${OUTPUT_PATH}
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run mongodb -s -threads 2 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 2> /dev/null | egrep "${PATTERN}" > ${OUTPUT_PATH}

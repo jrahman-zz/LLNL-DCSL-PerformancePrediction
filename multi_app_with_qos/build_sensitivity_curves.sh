15b6984cc3366d3264c5448a147c00e80fc8b0dd
jrahman <rahmanj@purdue.edu>
2015-12-02 22:20:08 -0800

    Update the rep count

--- multi_app_with_qos/build_sensitivity_curves.sh
+++ multi_app_with_qos/build_sensitivity_curves.sh
@@ -6,7 +6,7 @@ OUTPUT_DIR="/p/lscratche/${USER}/job_output"
 mkdir -p "${OUTPUT_DIR}"
 OUTPUT_PATH="${OUTPUT_DIR}/build_sensitivity_curve-%j-%2t.out"
 
-for REP in `seq 1 10`; do
+for REP in `seq 1 20`; do
     while read LINE; do
         IFS=' ' read -r -a QOS <<< "${LINE}"
         OUTPUT_FILE="${QOS[0]}.${REP}.sh"

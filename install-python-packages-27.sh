a0160e48d5c269078900a16175a3b89218da152d
jrahman <rahmanj@purdue.edu>
2015-12-13 12:03:02 -0800

    Added new script to install python2.7 with libraries

---
+++ install-python-packages-27.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+/usr/apps/python2.7.1/bin/virtualenv ~/py27
+
+~/py27/bin/pip install Flask
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to install Flask"
+    exit 1
+fi
+
+~/py27/bin/pip install numpy
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to install numyp"
+    exit 2
+fi
+
+~/py27/bin/pip install pandas
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to install pandas"
+    exit 3
+fi
+
+git clone git://github.com/matplotlib/matplotlib.git
+if [ $? -ne 0 ]; then
+    exit 4
+fi
+cd matplotlib
+python setup.py install
+if [ $? -ne 0 ]; then
+    cd ..
+    rm -rf matplotlib
+    exit 5
+fi
+cd ..
+rm -rf matplotlib
+
+~/py27/bin/pip install scipy
+if [ $? -ne 0 ]; then
+    exit 6
+fi
+
--- modelling/matrix_model.py
+++ modelling/matrix_model.py
@@ -1,13 +1,16 @@
-#!/bin/env python
+#!/usr/apps/python3.4.2/bin/python3
 
 import numpy as np
 import numpy.linalg as npla
 import numpy.matlib as npmat
 import pandas as pd
+import seaborn as sns
+import matplotlib
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
 
 import itertools
 import util
-
 
 """
 Attempt at a simpler scaled linear sum model
@@ -48,6 +51,59 @@ bubble_type = 'mean_bubble'
 sample_fractions = [0.2]
 sizes = [2, 3]
 
+def dist_plot(naive_data, pred_data, naive_label, pred_label, filename):
+    sns.distplot(naive_data, kde=False, rug=False, label=naive_label, axlabel='100*(observed_bubble - prediction)/observed_bubble')
+    sns.distplot(pred_data, kde=False, rug=False, label=pred_label, axlabel='100*(observed_bubble - prediction)/observed_bubble')
+    plt.legend()
+    plt.savefig(filename)
+    plt.close('all')
+
+def dist_plotting(config, data, apps):
+    for size in config:
+        pruned = data[data['app_count'] == size]
+        naive_error = 100 * (pruned[bubble_type] - pruned['naive_sum_bubble']) / pruned[bubble_type]
+        pred_error = 100 * (pruned[bubble_type] - pruned['pred_bubble']) / pruned[bubble_type]
+
+        filename = ':'.join(['%s=%s' % (str(size), str(fraction)) for size, fraction in config.items()])
+        filename = 'plot_' + filename
+        dist_plot(naive_error, pred_error, 'Naive Error', 'Model Error', '%(filename)s:app_count=%(size)s.dist.pdf' % locals())
+
+    for app in apps:
+        pruned = data[data[app] > 0]
+        naive_error = 100 * (pruned[bubble_type] - pruned['naive_sum_bubble']) / pruned[bubble_type]
+        pred_error = 100 * (pruned[bubble_type] - pruned['pred_bubble']) / pruned[bubble_type]
+
+        filename = ':'.join(['%s=%s' % (str(size), str(fraction)) for size, fraction in config.items()])
+        filename = 'plot_' + filename
+        dist_plot(naive_error, pred_error, 'Naive Error', 'Model Error', '%(filename)s:app=%(app)s.dist.pdf' % locals())
+
+def curve_plot(data, metric, fraction, label, filename):
+    d = data.sort(fraction)
+    sns.pointplot(data=d, estimator=np.median, y=metric, x=fraction, join=True)
+    plot.ylabel(label)
+    plt.legend()
+    plt.savefig(filename)
+    plt.close('all')
+
+def curve_plotting(configurations, data, apps):
+    """
+    Plot learning curves based on training set size
+        configurations:
+        data:
+        apps:
+    """
+    sizes = [size for size in configurations]
+    # For now, trim to only include situations with equal fractions per co-location count
+    for i in range(0, len(sizes) - 1):
+        size_a = sizes[i]
+        size_b = sizes[i + 1]
+        data = data[data['%(size_a)d_fraction' % locals()] == data['%(size_b)d_fraction' % locals()]]       
+    for metric in ['mean_error', 'median_error', 'max_error', 'std']:
+        filename = 'metric:%(metric)s.learning_curve.pdf' % locals()
+        fraction='2_fraction'
+        label = metric
+        curve_plot(data, metric, fraction, label, filename)
+
 def evaluate(config, data, apps):
     config_prefix = ':'.join(['%(key)d=%(value)f' % locals() for key, value in config.items()])
     model_types = {'pred_bubble': 'MM', 'naive_sum_bubble': 'NS'}
@@ -85,6 +141,7 @@ def evaluate(config, data, apps):
             print('%(model_prefix)s,app=%(app)s,max_error,%(max_error)f' % locals())
             print('%(model_prefix)s,app=%(app)s,std,%(std)f' % locals())
 
+
 def main():
  
     # Create fraction configurations
@@ -92,6 +149,11 @@ def main():
     fractions = itertools.product(*fracs)
     configurations = [{sizes[i]: tup[i] for i in range(len(sizes))} for tup in fractions]
 
+    # Dictionary to hold error data for learning curves
+    error_data = {'%(size)d_fraction' % locals(): [] for size in sizes}
+    for metric in ['mean_error', 'median_error', 'max_error', 'std']:
+        error_data[metric] = []
+    
     # Raw Data
     bubble_sizes, none = util.read_single_app_bubbles('single_bubble_sizes')
     data = util.read_data('experiment_data')
@@ -180,7 +242,21 @@ def main():
         data['pred_bubble'] = pred
         data['naive_sum_bubble'] = naive
 
+        # Track stats across multiple sample sizes
+        error = data[bubble_type] - data['pred_bubble']
+
+        for size, fraction in configuration.items():
+            error_data['%(size)d_fraction' % locals()].append(fraction)
+        error_data['mean_error'].append(np.mean(error))
+        error_data['median_error'].append(np.median(error))
+        error_data['max_error'].append(np.max(np.abs(error)))
+        error_data['std'].append(np.std(error))
+
+        dist_plotting(configuration, data, apps)
         evaluate(configuration, data, apps)
+    
+    error_data = pd.DataFrame(error_data)
+    curve_plotting(configurations, error_data, apps)
 
 if __name__ == '__main__':
     main()

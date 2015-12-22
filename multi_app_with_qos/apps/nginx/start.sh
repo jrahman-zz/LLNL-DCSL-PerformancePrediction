7734bb80be16d9278706f4807c0e4b2f60536c63
jrahman <rahmanj@purdue.edu>
2015-12-22 15:35:53 -0800

    Updated nginx script to block

--- multi_app_with_qos/apps/nginx/start.sh
+++ multi_app_with_qos/apps/nginx/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+DATA_DIR=$1
+
+PID_FILE="${DATA_DIR}/app.pid"
+ERROR_FILE="${DATA_DIR}/error.log"
+
 NGINX_DIR="/p/lscratche/${USER}/apps/nginx-1.8.0"
 
-$NGINX_DIR/sbin/nginx -c conf/nginx_non_root.conf
+$NGINX_DIR/sbin/nginx -c "conf/nginx_non_root.conf" -g "pid ${PID_FILE}; error_log ${ERROR_FILE};"
+
+# Wait for PID file to be populated, if nginx process is HIGHLY delayed there is a non-zero chance for a race
+sleep 2
+
+# NGINX will remove it's own PID file for us
+while [ -f "${PID_FILE}" ]; do
+    sleep 2
+done

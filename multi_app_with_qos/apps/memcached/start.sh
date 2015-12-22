ef0370a746164fbd0e230f80520cd2e96d5a2a82
jrahman <rahmanj@purdue.edu>
2015-12-22 14:58:01 -0800

    Updates to start.sh script for memcached

--- multi_app_with_qos/apps/memcached/start.sh
+++ multi_app_with_qos/apps/memcached/start.sh
@@ -4,9 +4,4 @@ DATA_DIR=$1
 
 MEMCACHED_DIR="/p/lscratche/${USER}/apps/memcached/install"
 
-${MEMCACHED_DIR}/bin/memcached -d -m 1024 -u root -l 127.0.0.1 -p 11211 -P "${DATA_DIR}/app.pid"
-
-# Jason: This start script needs to block upon starting the QoS application
-# Does the -d flag result in the memcached daemon forking and allowing the script to continue?
-# If so a call to wait needs to be added here so this script blocks
-# Also, stdout and stderr surpression *might* need to be added
+${MEMCACHED_DIR}/bin/memcached -u ${USER} -m 1024 -l 127.0.0.1 -p 11211 -P "${DATA_DIR}/app.pid"

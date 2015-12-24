0725c30704935f9a7374209cc824f900c2bb886c
jrahman <rahmanj@purdue.edu>
2015-12-24 12:01:36 -0800

    Update memcached scripts

--- multi_app_with_qos/apps/memcached/load.sh
+++ multi_app_with_qos/apps/memcached/load.sh
@@ -5,5 +5,7 @@ YCSB_DIR=$APPS_DIR/YCSB_with_memcache/YCSB
 
 WORKLOAD=$1
 
+cd "${YCSB_DIR}"
+
 echo "**** Loading Memcached with YCSB **** with workload = ${WORKLOAD}"
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load memcached -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 -p "memcached.hosts=127.0.0.1" 2>/dev/null 1>/dev/null
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load memcached -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 -p "memcached.hosts=127.0.0.1" 
--- multi_app_with_qos/apps/memcached/run_driver.sh
+++ multi_app_with_qos/apps/memcached/run_driver.sh
@@ -5,11 +5,10 @@ YCSB_DIR=$APPS_DIR/YCSB_with_memcache/YCSB
 
 WORKLOAD=$1
 OUTPUT_PATH=$2
+ABS_OUTPUT_PATH=`cd "${OUTPUT_PATH}"; pwd`
 
 # Filter irrelevant output we don't care about
 PATTERN="\[OVERALL\]|\[CLEANUP\]|\[READ\]|\[UPDATE\]"
-
+cd "${YCSB_DIR}"
 echo "**** Running Memcached with YCSB **** with workload = ${WORKLOAD}"
-
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run memcached -s -threads 8 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 -p "memcached.hosts=127.0.0.1" 2>/dev/null 1>/dev/null | egrep "${PATTERN}" > ${OUTPUT_PATH}
-
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run memcached -s -threads 8 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 -p "memcached.hosts=127.0.0.1" | egrep "${PATTERN}" > ${ABS_OUTPUT_PATH}

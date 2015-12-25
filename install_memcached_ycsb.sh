d8d41b96e3ed401c2166b0cb3c5836765c9fa612
jrahman <rahmanj@purdue.edu>
2015-12-24 18:10:17 -0800

    Updates for installation and running of YCSB with memcached

--- install_memcached_ycsb.sh
+++ install_memcached_ycsb.sh
@@ -2,14 +2,27 @@
 
 APPS_DIR=/p/lscratche/${USER}/apps
 MEMCACHED=$APPS_DIR/memcached
-YCSBDIR="${APPS_DIR}/YCSB_with_memcache/"
+YCSBSOURCE="${APPS_DIR}/YCSB_with_memcache/"
+YCSB_VERSION="0.7.0-SNAPSHOT"
+YCSBDIR="${APPS_DIR}/ycsb-${YCSB_VERSION}"
 
 if [ ! -d "${YCSBDIR}" ]; then
-    mkdir -p $YCSBDIR
-    cd $YCSBDIR
+    mkdir -p $YCSBSOURCE
+    cp ycsb.pom ${YCSBSOURCE}
+    cd $YCSBSOURCE
+    rm -rf YCSB
     git clone https://github.com/brianfrankcooper/YCSB.git
+    mv ycsb.pom YCSB/pom.xml
     cd YCSB/
-    mvn -pl com.yahoo.ycsb:memcached-binding -am clean package
+    mvn clean package
+    cp distribution/target/ycsb-${YCSB_VERSION}.tar.gz "${APPS_DIR}"
+    cd "${APPS_DIR}"
+    tar -zxvf "ycsb-${YCSB_VERSION}.tar.gz"
+fi
+
+if [ ! -d "${YCSBDIR}" ]; then
+    echo "ERROR: Failed to install YCSB"
+    exit 1
 fi
 
 if [ ! -d "${MEMCACHED}" ]; then
--- multi_app_with_qos/apps/memcached/run_driver.sh
+++ multi_app_with_qos/apps/memcached/run_driver.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 APPS_DIR=/p/lscratche/${USER}/apps
-YCSB_DIR=$APPS_DIR/YCSB_with_memcache/YCSB
+YCSB_DIR=$APPS_DIR/ycsb-0.7.0-SNAPSHOT
 
 WORKLOAD=$1
 OUTPUT_PATH=$2
-ABS_OUTPUT_PATH=`cd "${OUTPUT_PATH}"; pwd`
+ABS_OUTPUT_PATH=`echo "$(cd "$(dirname "${OUTPUT_PATH}")"; pwd)/$(basename "${OUTPUT_PATH}")"`
 
 # Filter irrelevant output we don't care about
 PATTERN="\[OVERALL\]|\[CLEANUP\]|\[READ\]|\[UPDATE\]"
-cd "${YCSB_DIR}"
 echo "**** Running Memcached with YCSB **** with workload = ${WORKLOAD}"
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run memcached -s -threads 8 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=5000000 -p "memcached.hosts=127.0.0.1" | egrep "${PATTERN}" > ${ABS_OUTPUT_PATH}
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb run memcached -s -threads 8 -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=1000000 -p "memcached.hosts=127.0.0.1" 2>&1 | egrep "${PATTERN}" > ${ABS_OUTPUT_PATH}

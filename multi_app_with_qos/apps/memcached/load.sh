7f776005542a4476128b61a29aace4fb2c8db3a4
jrahman <rahmanj@purdue.edu>
2015-12-27 17:10:42 -0800

    Update memcached and mongodb with fixed throughput values

--- multi_app_with_qos/apps/memcached/load.sh
+++ multi_app_with_qos/apps/memcached/load.sh
@@ -6,4 +6,4 @@ YCSB_DIR=$APPS_DIR/ycsb-0.7.0-SNAPSHOT
 WORKLOAD=$1
 
 echo "**** Loading Memcached with YCSB **** with workload = ${WORKLOAD}"
-/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load memcached -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=1000000 -p "memcached.hosts=127.0.0.1" 
+/usr/apps/python2.7.10/bin/python ${YCSB_DIR}/bin/ycsb load memcached -s -P "${YCSB_DIR}/workloads/${WORKLOAD}" -p recordcount=500000 -p operationcount=3000000 -p "memcached.hosts=127.0.0.1" > /dev/null 2> /dev/null
--- multi_app_with_qos/apps/memcached/start.sh
+++ multi_app_with_qos/apps/memcached/start.sh
@@ -4,4 +4,4 @@ DATA_DIR=$1
 
 MEMCACHED_DIR="/p/lscratche/${USER}/apps/memcached/install"
 
-${MEMCACHED_DIR}/bin/memcached -u ${USER} -m 1024 -l 127.0.0.1 -p 11211 -P "${DATA_DIR}/app.pid"
+${MEMCACHED_DIR}/bin/memcached -u ${USER} -t 1 -m 1024 -l 127.0.0.1 -p 11211 -P "${DATA_DIR}/app.pid"

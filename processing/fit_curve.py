3dbeefdf11ad7b3cf86ea02e5decf4f447b7b1f4
jrahman <rahmanj@purdue.edu>
2016-01-04 15:41:42 -0800

    Update to fit curve to use IsotonicRegression

--- processing/fit_curve.py
+++ processing/fit_curve.py
@@ -6,22 +6,27 @@ import numpy as np
 import sys
 import utils
 
+from sklearn.IsotonicRegression import IsotonicRegression
+
 #
 # Fit a curve to the bubble_size-ipc curve
 # fit_curve.py
 #
 
 def fit_curve(x, y):
-    return interpolate.interp1d(x, y, kind=1)    
+    curve = IsotonicRegression(increasing=False)
+    curve.fit(x, y)
+    return curve
+    #return interpolate.interp1d(x, y, kind=1)    
 
 def predict(curve, x):
-    return curve(x)
+    return curve.predict([x])[0]
 
 def plot_fit(curve, x, y):
     max_x = max(x)
     min_x = min(x)
     xnew = np.arange(min_x, max_x)
-    ynew = curve(xnew)
+    ynew = curve.predict(xnew)
     plt.plot(x, y, 'o', xnew, ynew, '-')
     plt.show()
             

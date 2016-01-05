c14e16a26baaa5b3c18a51a06142180e10349868
jrahman <rahmanj@purdue.edu>
2016-01-05 11:56:23 -0800

    Update bubble construction script

--- build-bubbles.sh
+++ build-bubbles.sh
@@ -14,6 +14,8 @@ do
   gcc "${file}" -lrt -lpthread -o "bin/${base_f%.c}"
 done
 
+gcc "src/bubbles/bubble.c" -O1 -lrt -lpthread -o "bin/bubble"
+
 for SIZE in 524288 1048576 2097152 4194304 8338608 16777216 33554423
 do
   echo "Building reporter of size ${SIZE}"

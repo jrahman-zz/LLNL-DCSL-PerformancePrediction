9b0ddcaba1b0eb938a1981dc5007258c11fc1d24
jrahman <rahmanj@purdue.edu>
2016-01-09 11:17:10 -0800

    Updated processing to handle all steps automatically

--- single_app_contention/process.sh
+++ single_app_contention/process.sh
@@ -4,6 +4,21 @@
 # Given file containing raw completed experiement data, process and plot it
 #
 
+cat experiment_list.* > experiment_list
+./process_experiments.sh experiment_list > raw_data
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to process experiments"
+    exit 1
+fi
+
 ./process_raw_data.py raw_data | sort -rn > processed_data
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to process raw data"
+    exit 2
+fi
+
 ./plot_bubble_sizes
-../plot_reporter_curve
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to plot bubble sizes"
+    exit 3
+fi
